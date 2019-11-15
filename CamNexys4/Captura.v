03b164d8f8d54253fed7c400e25ace471441399c
julian <37085956+jdpulidoca@users.noreply.github.com>
2019-11-15 20:55:17 +0100

    Ajuste image.men y correcion Captura.v

--- CamNexys4/Captura.v
+++ CamNexys4/Captura.v
@@ -23,26 +23,36 @@ module Captura(
     input Href,
     input Vsync,
     input Pclk,
-    output reg regWrite,
-    output reg addr_in,
-	 output reg data_in[7:0]
+    output regWrite,
+    output reg [16:0]addr_in,
+	 output reg [7:0]data_in
     );
-	 wire contador=0;
+	 reg contador=0;
+	 reg w=0;
+	 reg [16:0]addr=0;
 always @ (negedge Pclk) begin
-	if	(Vsync==1) begin
+	if	(Vsync==0) begin
 		if	(Href==1) begin
+		w=1;
 			if	(contador==0)begin
-assign data_in[7:5]={Data[7:5]};
-assign data_in[4:2]={Data[2:0]};
-contador=1;
+				data_in[7]=Data[7];
+				data_in[6]=Data[6];
+				data_in[5]=Data[5];
+				data_in[4]=Data[2];
+				data_in[3]=Data[1];
+				data_in[2]=Data[0];
+				contador=1;
 			end
 			if	(contador==1)begin
-assign data_in[1:0]={Data[4:3]};
-contador=0;
+				data_in[1]=Data[4];
+				data_in[0]=Data[3];
+				contador=0;
+				w=0;
+				addr=addr+1;
 			end
+		end
+	end
 end
-end
-
-end
+assign regWrite=w;
 
 endmodule
